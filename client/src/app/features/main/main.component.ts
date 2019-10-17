cc13a33697ac5308d305c39fb055a48e769cd0c8
Vitaliy Nesterenko <vith@yandex.ru>
2019-10-17 12:17:20 +0500

    главная страница

--- client/src/app/features/main/main.component.ts
+++ client/src/app/features/main/main.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { Contest, emptyContest } from '../../core/types/contest';
+import { Photowork } from '../../core/types/photowork';
+
+interface ContestInfo {
+  contest: Contest,
+  photoworks: Array<Photowork>
+};
 
 
 @Component({
@@ -7,9 +15,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.less']
 })
 export class MainComponent {
+  contest: Contest = emptyContest;
+  photoworks: Array<Photowork> = [];
+  image?: string;
+  currentImage = 0;
 
+  constructor(private api: ApiService) { }
 
+  ngOnInit() {
+    this.api.get<ContestInfo>("api/contests/results").subscribe(({ contest, photoworks }) => {
+      this.contest = contest;
+      this.photoworks = photoworks;
+      this.image = this.photoworks[this.currentImage].filename;
+    });
+  }
 
+  next() {
+
+    this.currentImage = (this.currentImage + 1) % this.photoworks.length;
+    this.image = this.photoworks[this.currentImage].filename;
+  }
+
+  prev() {
+    this.currentImage = (this.currentImage - 1);
+    if (this.currentImage < 0) {
+      this.currentImage = this.photoworks.length - 1;
+    }
+    this.image = this.photoworks[this.currentImage].filename;
+  }
 
 
 
--- server/routes/contests.js
+++ server/routes/contests.js
@@ -2,6 +2,8 @@ const Router = require("koa-router");
 const router = new Router();
 const models = require("../../models");
 const R = require("ramda");
+const getUploadFilePath = require("../utils/getUploadPath");
+
 
 
 router.get("/", async ctx => {
@@ -26,6 +28,45 @@ router.get("/", async ctx => {
   });
 });
 
+router.get('/results', async ctx => {
+  const { host } = ctx.request.header;
+  const [domain, port] = host.split(":");
+  const query = `
+    select contests.id, salones.name, subname, domain, design_code 
+    from salones, contests 
+    where contests.salone_id=salones.id and salones.domain=:domain
+  `;
+
+  const [[contest]] = await models.sequelize.query(query, {
+    replacements: {
+      domain
+    }
+  });
+
+  const worksQuery = `
+    select photoworks.id, filename 
+    from photoworks, sections, contests
+    where photoworks.section_id=sections.id and sections.contest_id=contests.id
+    and contests.id=:contestId
+  `;
+
+  let [photoworks] = await models.sequelize.query(worksQuery, {
+    replacements: {
+      contestId: contest.id
+    }
+  });
+
+  photoworks = photoworks.map(p => {
+    return {
+      id: p.id,
+      filename: getUploadFilePath(p.filename)
+    };
+  });
+
+  ctx.body = { contest, photoworks };
+});
+
+
 router.get("/:id", async ctx => {
   const { id } = ctx.params;
   const contest = await models.Contest.findOne({
@@ -35,6 +76,7 @@ router.get("/:id", async ctx => {
   })
   ctx.body = contest;
 });
+
 
 
 module.exports = router;
