ae16f4faa57cb3d967a63fc672b902a695e04645
Vitaliy Nesterenko <vith@yandex.ru>
2019-11-21 11:03:29 +0500

    Вывод на главную страницу информации о салоне и конкурсе

--- server/routes/contests.js
+++ server/routes/contests.js
@@ -1,12 +1,11 @@
-const Router = require("koa-router");
+const Router = require('koa-router');
 const router = new Router();
-const models = require("../../models");
-const R = require("ramda");
-const getUploadFilePath = require("../utils/getUploadPath");
+const models = require('../../models');
+const R = require('ramda');
+const getUploadFilePath = require('../utils/getUploadPath');
+const camelizeObject = require('../utils/camelizeObject');
 
-
-
-router.get("/", async ctx => {
+router.get('/', async ctx => {
   const userId = ctx.user.id;
   const query = `
     select contests.id, subname, date_start, date_stop, salones.name as salone, reg_state, contests.section_count, maxrate from salones, contests
@@ -32,13 +31,13 @@ router.get("/", async ctx => {
       canPostPhotos: c.reg_state === 1,
       sectionCount: c.section_count,
       maxrate: c.maxrate
-    }
+    };
   });
 });
 
 router.get('/results', async ctx => {
   const { host } = ctx.request.header;
-  const [domain, port] = host.split(":");
+  const [domain] = host.split(':');
   const query = `
     select contests.id, salones.name, subname, domain, design_code 
     from salones, contests 
@@ -74,15 +73,36 @@ router.get('/results', async ctx => {
   ctx.body = { contest, photoworks };
 });
 
-
-router.get("/:id", async ctx => {
+router.get('/:id', async ctx => {
   const { id } = ctx.params;
   const contest = await models.Contest.findOne({
     where: {
       id
     }
-  })
+  });
   ctx.body = contest;
+});
+
+router.get('/about/:lang', async ctx => {
+  const { lang } = ctx.params;
+  const { host } = ctx.request.header;
+  const [domain] = host.split(':');
+  const query = `
+    select contest_abouts.name as contest_name, salone_abouts.name as salone_name, contests.years
+    from salones, contests, contest_abouts, salone_abouts, languages
+    where contests.salone_id=salones.id and salones.domain=:domain and contest_abouts.contest_id=contests.id  and salone_abouts.salone_id=salones.id and
+    contest_abouts.language_id=languages.id and salone_abouts.language_id=languages.id and languages.short=:lang
+  `;
+
+  const [result] = await models.sequelize.query(query, {
+    replacements: {
+      domain,
+      lang
+    }
+  });
+
+  const about = R.head(R.flatten(result));
+  ctx.body = about && camelizeObject(about);
 });
 
 module.exports = router;
