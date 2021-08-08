a03c5d87a3fdd9b777f2949427eb7ff758c7f828
Nesterenko Vitaliy <vith@yandex.ru>
2021-08-08 20:45:48 +0500

    lint configs

---
+++ react-client/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-unused-vars": ["off", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "no-debugger": "warn",
+    "no-var-requires": ["off"],
+    "react/prop-types": "off"
+  }
+};
+
