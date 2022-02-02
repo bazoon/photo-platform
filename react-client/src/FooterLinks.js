aac54b84776a13d8ef1ca59d87008448df2b0261
Nesterenko Vitaliy <vith@yandex.ru>
2022-02-02 21:54:28 +0500

    fix

--- react-client/src/FooterLinks.js
+++ react-client/src/FooterLinks.js
@@ -6,12 +6,12 @@ function FooterLinks(){
   const { t } = useTranslation("namespace1");
   return(
     <div className="grid gap-2 text-lg grid-cols-2 grid-rows-3">
-      <Link className="text-gray2 no-underline hover:underline" to="/thesis">{t("about-us")}</Link>
-      <Link className="text-gray2 no-underline hover:underline" to="/partners">{t("partners")}</Link>
-      <Link className="text-gray2 no-underline hover:underline" to="/organizers">{t("organizers")}</Link>
-      <Link className="text-gray2 no-underline hover:underline" to="/jury">{t("jury")}</Link>
-      <Link className="text-gray2 no-underline hover:underline" to="/rules">{t("rules")}</Link>
-      <Link className="text-gray2 no-underline hover:underline" to="/politics">{t("politics")}</Link>
+      <Link className="text-gray2 no-underline hover:underline" to="">{t("about-us")}</Link>
+      <Link className="text-gray2 no-underline hover:underline" to="">{t("partners")}</Link>
+      <Link className="text-gray2 no-underline hover:underline" to="">{t("organizers")}</Link>
+      <Link className="text-gray2 no-underline hover:underline" to="">{t("jury")}</Link>
+      <Link className="text-gray2 no-underline hover:underline" to="">{t("rules")}</Link>
+      <Link className="text-gray2 no-underline hover:underline" to="">{t("politics")}</Link>
     </div>
   );
 }
